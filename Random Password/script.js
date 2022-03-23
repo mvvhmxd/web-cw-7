1c6d53ac31f724a0059130a6c429d6dc6d0dfc10
Mahmoud Mohamed Balash <mbh27176@gmail.com>
2022-03-24 01:50:09 +0300

    d

--- Random Password/script.js
+++ Random Password/script.js
@@ -1,3 +1,42 @@
 // Database قاعدة البيانات
+const chars = [
+    {
+lsmall:"asdfghjklrdyytgvjvh"
+    },
+
+   {lcap:"RDTFYGUHLIKHCDNVH"
+},
+
+{
+    num:"98765434567778"
+},
+
+{charcs:"*#!@#$%($#^%#$#####_~"
+
+},
+];
 
 // دالة generate
+   function generate() {
+    let randompassword = "";
+    let collection_1  = chars[0] ["lsmall"];
+    let collection_2  = chars[1] ["lcap"];
+    let collection_3  = chars[2] ["num"];
+    let collection_4  = chars[3] ["charcs"];
+    
+    randompassword =
+
+    randompassword + 
+    collection_1.charAt(Math.floor(Math.random * collection_1.length));
+
+    randompassword + 
+    collection_2.charAt(Math.floor(Math.random * collection_2.length));
+
+    randompassword + 
+    collection_3.charAt(Math.floor(Math.random * collection_3.length));
+
+    randompassword + 
+    collection_4.charAt(Math.floor(Math.random * collection_4.length));
+    display.value = randompassword;
+   
+   }
--- Random/script.js
+++ Random/script.js
@@ -1,3 +1,19 @@
 // project أنشئ كائن
+const project = {
+    type:"health",
+    user:23,
+    biography() {  return "i help people be healthy"
+    },
 
+};
 // randomProperty أنشئ دالة
+function RandomProperty(obj) {
+    const keys = Object.keys(obj);
+  
+    const random = Math.floor(Math.random() * keys.length);
+  
+  
+console.log(keys[random]);
+console.log(obj[`${keys[random]}`]);
+}
+RandomProperty(project);
